b53eae722047298e4f65f886153cacc68b5aab97
hritikbk07 <hritikkami56@gmail.com>
2026-01-14 18:57:27 +0545

    products details

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProductsPage from './products/page'
 
 const MainPage = () => {
   return (
-    <div>
-      <h1>MainPage</h1>
+    <div className="min-h-screen bg-gray-100 mt-17">
+      <ProductsPage />
     </div>
   )
 }
--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -12,10 +12,10 @@ const ProductsPage = () => {
   }, [])
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">
+    <div className="p-6 ">
+      {/* <h1 className="text-3xl font-bold mb-6 text-center">
         Products
-      </h1>
+      </h1> */}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map(product => (
